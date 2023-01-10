aee3bac936b1ae0492f381d978c3d9845322f937
ioj4 <69911332+ioj4@users.noreply.github.com>
2023-01-10 15:34:31 +0100

    Refactor

--- plugins/prevent-spotify-pause/index.js
+++ plugins/prevent-spotify-pause/index.js
@@ -1,22 +1,14 @@
-let unpatch;
+// disable notice
+shelter.flux.stores.NoticeStore.__getLocalVars().temporaryDismisses["SPOTIFY_AUTO_PAUSED"] = true;
 
-export function onLoad() {
-    // disable notice
-    shelter.flux.stores.NoticeStore.__getLocalVars().temporaryDismisses["SPOTIFY_AUTO_PAUSED"] = true
-
-    // prevent access to the pause endpoint
-    unpatch = shelter.patcher.instead(
-        "send",
-        XMLHttpRequest.prototype,
-        function (args, orig) {
-          const testUrl = /https?:\/\/api.spotify.com.+\/me\/player\/pause/;
-          if (!testUrl.test(this.__sentry_xhr__?.url)) {
-            return orig.apply(this, args);
-          }
-        }
-    );
-}
-
-export function onUnload() {
-    unpatch();
-}
+// prevent access to the pause endpoint
+export const onUnload = shelter.patcher.instead(
+  "send",
+  XMLHttpRequest.prototype,
+  function (args, orig) {
+    const testUrl = /https?:\/\/api.spotify.com.+\/me\/player\/pause/;
+    if (!testUrl.test(this.__sentry_xhr__?.url)) {
+      return orig.apply(this, args);
+    }
+  }
+);
