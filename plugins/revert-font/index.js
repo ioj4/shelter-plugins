948798c1d0f9e7ba6fcfe3428b5b888f19faea02
ioj4 <69911332+ioj4@users.noreply.github.com>
2023-03-25 18:30:05 +0100

    formatting

--- plugins/revert-font/index.js
+++ plugins/revert-font/index.js
@@ -1,2 +1,2 @@
-// Credits to https://overimagine1.github.io/old-discord-font for the CSS
+// credits to https://overimagine1.github.io/old-discord-font for the CSS
 export const onUnload = shelter.ui.injectCss("@import url(https://cdn.jsdelivr.net/gh/Overimagine1/old-discord-font/source.min.css);");
