a6801bb7691ba85708e81191708b30ec6da80f9d
ioj4 <69911332+ioj4@users.noreply.github.com>
2023-03-25 17:37:36 +0100

    vc-timer: initial commit

---
+++ plugins/vc-timer/index.jsx
@@ -0,0 +1,30 @@
+const {
+	flux: { dispatcher },
+    ui: { injectCss }
+} = shelter;
+
+import Timer from "./components/timer";
+import { css } from "./components/timer.jsx.scss"
+
+function addTimer() {
+    document.querySelector(`[class*="connection-"] > div[class*="inner-"] > div > a > div[class*="subtext"]`)
+        .prepend(<Timer/>);
+}
+
+function onDispatch(e) {
+    if (e.event === "join_voice_channel") {
+        addTimer();
+    }
+}
+
+let uninject;
+
+export function onLoad() {
+    uninject = injectCss(css);
+    dispatcher.subscribe("TRACK", onDispatch);
+}
+
+export function onUnload() {
+    dispatcher.unsubscribe("TRACK", onDispatch);
+    uninject();
+}
