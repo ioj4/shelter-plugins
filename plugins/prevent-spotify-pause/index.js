9c0a770e9a2cc95bbd38f96129fdcf688786fb75
ioj4 <69911332+ioj4@users.noreply.github.com>
2023-03-25 20:47:50 +0100

    prevent-spotify-pause: format

--- plugins/prevent-spotify-pause/index.js
+++ plugins/prevent-spotify-pause/index.js
@@ -1,14 +1,20 @@
-// disable notice
-shelter.flux.stores.NoticeStore.__getLocalVars().temporaryDismisses[
+const {
+    flux: { stores: { NoticeStore } },
+    patcher
+} = shelter;
+
+// dismiss popup in advance
+NoticeStore.__getLocalVars().temporaryDismisses[
     'SPOTIFY_AUTO_PAUSED'
 ] = true;
 
+const testUrl = /https?:\/\/api.spotify.com.+\/me\/player\/pause/;
+
 // prevent access to the pause endpoint
-export const onUnload = shelter.patcher.instead(
+export const onUnload = patcher.instead(
 	'send',
     XMLHttpRequest.prototype,
     function (args, orig) {
-        const testUrl = /https?:\/\/api.spotify.com.+\/me\/player\/pause/;
         if (!testUrl.test(this.__sentry_xhr__?.url)) {
             return orig.apply(this, args);
         }
