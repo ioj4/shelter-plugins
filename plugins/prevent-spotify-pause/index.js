e8e2ea9728ae6cc2ddb296b23821f1aa65c5d68b
ioj4 <69911332+ioj4@users.noreply.github.com>
2023-04-30 11:47:20 +0200

    prevent-spotify-pause: more reliable

--- plugins/prevent-spotify-pause/index.js
+++ plugins/prevent-spotify-pause/index.js
@@ -1,12 +1,35 @@
 const {
     flux: {
-        stores: { NoticeStore }
+        stores: { NoticeStore, VoiceStateStore },
+        dispatcher
     },
     patcher
 } = shelter;
 
+// accessing the NoticeStore too early throws an error sometimes
+// so wait until the latest possible moment (when a vc is joined)
+function awaitVoiceChannel() {
+    return new Promise((resolve) => {
+        if (VoiceStateStore.isCurrentClientInVoiceChannel()) {
+            resolve();
+            return;
+        }
+        const onDispatch = (payload) => {
+            if (payload?.event === "join_voice_channel") {
+                resolve();
+                dispatcher.unsubscribe("TRACK", onDispatch);
+            }
+        };
+        dispatcher.subscribe("TRACK", onDispatch);
+    });
+}
+
 // dismiss popup in advance
-NoticeStore.__getLocalVars().temporaryDismisses["SPOTIFY_AUTO_PAUSED"] = true;
+awaitVoiceChannel().then(() => {
+    NoticeStore.__getLocalVars().temporaryDismisses[
+        "SPOTIFY_AUTO_PAUSED"
+    ] = true;
+});
 
 const testUrl = /https?:\/\/api.spotify.com.+\/me\/player\/pause/;
 
