c8922541aa45250ed232e9470494d8005f404089
ImportTaste <53661808+ImportTaste@users.noreply.github.com>
2023-07-11 15:48:32 -0500

    capture all non-extension text in filename

--- plugins/timestamp-file-names/index.js
+++ plugins/timestamp-file-names/index.js
@@ -8,10 +8,23 @@ export function onLoad() {
     unintercept = intercept((dispatch) => {
         if (dispatch?.type === "UPLOAD_ATTACHMENT_ADD_FILES") {
             dispatch?.files?.forEach(({ file }) => {
-                // change the filename to current timestamp
+                // change the root name of the file to current epoch time
                 Object.defineProperty(file, "name", {
                     value: file?.name?.replace(
-                        /^([^.]+)/,
+                        // capture up to amount within curly bracket quantifier
+                        new RegExp(
+                            "^(.{" +
+                            (
+                                // check if filename includes a dot
+                                file.name.includes(".") ?
+                                // if so, use its last index in the quantifier
+                                file.name.lastIndexOf(".") :
+                                // if not, use filename length in the quantifier
+                                file.name.length
+                            )
+                            + "})"
+                        ),
+                        // denote timestamp as the new value for captured text
                         Date.now().toString()
                     )
                 });
