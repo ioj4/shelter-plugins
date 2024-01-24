051a1b652417e801cd16d48e97620d01aa00b27f
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-01-24 23:54:10 +0100

    [no-call-idle] resubscribe handlers on unload

--- plugins/no-call-idle/index.js
+++ plugins/no-call-idle/index.js
@@ -3,15 +3,23 @@ const {
 } = shelter;
 
 const dispatchTypes = ["EMBEDDED_ACTIVITY_DISCONNECT", "VOICE_STATE_UPDATES"];
+const resubscribe = [];
 
-export const onUnload = intercept((payload) => {
-    if (dispatchTypes.includes(payload?.type)) {
-        // delete handlers that start the call idle timeout
-        const actionHandlers = dispatcher?._subscriptions?.[payload.type];
+// delete subscription handler right before it gets called
+const unintercept = intercept(({ type }) => {
+    if (dispatchTypes.includes(type)) {
+        const actionHandlers = dispatcher._subscriptions[type] ?? [];
+
         for (const handler of actionHandlers) {
-            if (handler.toString().includes("idleTimeout.start(")) {
+            if (handler.toString().includes("idleTimeout.start")) {
                 actionHandlers.delete(handler);
+                resubscribe.push(() => actionHandlers.add(handler));
             }
         }
     }
 });
+
+export function onUnload() {
+    unintercept();
+    resubscribe.forEach((resub) => resub());
+}
