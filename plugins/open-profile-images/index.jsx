d0a646b86e89220599e691a0b630662eb356748f
ioj4 <69911332+ioj4@users.noreply.github.com>
2024-02-17 23:05:56 +0100

    [open-profile-images] rewrite +more avatar elements to click on

--- plugins/open-profile-images/index.jsx
+++ plugins/open-profile-images/index.jsx
@@ -1,84 +1,49 @@
+import ImageModal from "./components/image-modal";
+import classes from "./styles.jsx.scss";
+
 const {
-    plugin: { store },
-    flux: { dispatcher, intercept },
-    ui: { openModal },
-    observeDom
+    ui: { openModal }
 } = shelter;
 
-import ImageModal from "./components/image-modal";
+const scoped = shelter.plugin.scoped;
 
-function addClickEvent(userComponent) {
-    const banner = userComponent.querySelector(`div[class*="bannerPremium_"]`);
-    if (banner) {
-        banner.addEventListener("click", (e) =>
-            openImage(e?.target?.style?.backgroundImage?.slice(5, -2))
-        );
-        banner.style.cursor = "pointer";
-    }
-
-    userComponent
-        .querySelectorAll(`div[class*="wrapper_"]`)
-        .forEach((avatar) => {
-            // skip clickable element that opens the profile-modal (this is horrible)
-            avatarWrapper = avatar.parentElement.parentElement;
-            if (
-                avatarWrapper.className.includes("clickable_") &&
-                avatarWrapper.onclick
-            )
-                return;
-
-            avatar.addEventListener("click", (e) =>
-                openImage(e?.target?.querySelector(`img[class*="avatar"]`)?.src)
-            );
-            avatar.style.cursor = "pointer";
-        });
+function onClick(e) {
+    const src =
+        e.target?.querySelector(`img[class*="avatar"]`)?.src ??
+        e.target?.style?.backgroundImage?.slice(5, -2); // for banners, the slice removes the "url()" part
+    if (!src) return;
+    openModal(() => <ImageModal url={new URL(src)} />);
+    e.stopImmediatePropagation();
 }
 
-function openImage(urlString) {
-    if (!urlString) return;
-    openModal(() => <ImageModal url={new URL(urlString)} />);
-}
-
-const USER_COMPONENT_QUERY = `[class*="userProfileModalInner"],[class*="userPopoutInner"]`;
-
-function observeComponents() {
-    // wait for the component to be loaded
-    const unObserve = observeDom(USER_COMPONENT_QUERY, (userComponent) => {
-        unObserve();
-        queueMicrotask(addClickEvent.bind(null, userComponent));
-    });
-    setTimeout(unObserve, 1_000);
-}
-
-function onTrack(e) {
-    if (e.event === "open_popout" || e.event === "open_modal") {
-        observeComponents();
-    }
-}
-
-let unintercept;
+// this is fine 🔥
+// it really is, i tested perf 🪄
+const subSelectors = [
+    // own pfp in bottom left
+    `[class*="avatarWrapper"]:not([class*="clickable"])`,
+    // pfp in member list
+    `[class*="memberInner"] [class*="avatar"] [class*="wrapper"]`,
+    // pfp in popout of webhooks
+    `[class*="avatarWrapperNonUserBot"]`,
+    // pfp in profile modal and other places
+    `[class*="wrapper"][class*="avatar"]`,
+    // banner in profile modal
+    `[class*="bannerPremium"]:not([class*="settingsBanner"])`
+];
 
 export function onLoad() {
-    // prevent discord's context-menu from opening for the image
-    // using it is not possible as it's in a layer behind the modal
-    unintercept = intercept((dispatch) => {
-        if (
-            dispatch?.type === "CONTEXT_MENU_OPEN" &&
-            dispatch?.contextMenu?.target?.hasAttribute("ioj4-opi")
-        ) {
-            return false;
+    scoped.observeDom(
+        `:is(${subSelectors.join(",")}):not(.${classes.ioj4Opi})`,
+        (el) => {
+            el.classList.add(classes.ioj4Opi);
+            el.addEventListener("click", onClick);
         }
-    });
-    store.fullResolution ??= false;
-    dispatcher.subscribe("TRACK", onTrack);
-    // for webhook userpopouts (they always fail)
-    dispatcher.subscribe("USER_PROFILE_FETCH_FAILURE", observeComponents);
+    );
 }
 
 export function onUnload() {
-    dispatcher.unsubscribe("TRACK", onTrack);
-    dispatcher.unsubscribe("USER_PROFILE_FETCH_FAILURE", observeComponents);
-    unintercept();
+    document.querySelectorAll(`.${classes.ioj4Opi}`).forEach((el) => {
+        el.removeEventListener("click", onClick);
+        el.classList.remove(classes.ioj4Opi);
+    });
 }
-
-export { default as settings } from "./components/settings";
